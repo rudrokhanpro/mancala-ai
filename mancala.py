2e39fc68b6f7f6f7ba6644ff573080b4b582266b
Rudro KHAN <rudrokhan.pro@gmail.com>
2021-04-21 11:31:45 -0700

    Defined a Mancala class & show board method

--- mancala.py
+++ mancala.py
@@ -1,5 +1,38 @@
+class Mancala:
+    board = [4, 4, 4, 4, 4, 4, 0, 4, 4, 4, 4, 4, 4, 0]
+
+    PLAYER_MANCALA_INDEX = 6
+    BOT_MANCALA_INDEX = 13
+
+    def __init__(self):
+        pass
+
+    def show_board(self):
+        """
+            Intial board :
+                ( 4) ( 4) ( 4) ( 4) ( 4) ( 4)
+            ( 0)                               ( 0)
+                ( 4) ( 4) ( 4) ( 4) ( 4) ( 4)
+        """
+        player_pits = self.board[self.PLAYER_MANCALA_INDEX +
+                                 1:self.BOT_MANCALA_INDEX]
+
+        bot_pits = self.board[:self.PLAYER_MANCALA_INDEX]
+
+        mancalas = self.board[self.BOT_MANCALA_INDEX], self.board[
+            self.PLAYER_MANCALA_INDEX]
+
+        print("    (%2s) (%2s) (%2s) (%2s) (%2s) (%2s)" % tuple(bot_pits))
+        print("(%s)                               (%s)" % mancalas)
+        print("    (%2s) (%2s) (%2s) (%2s) (%2s) (%2s)" % tuple(player_pits))
+
+
 def main():
     print('Welcome to Mancala !')
+
+    game = Mancala()
+
+    game.show_board()
 
 
 if __name__ == '__main__':
