b62c1b1f0b2344e6dc6125681ce97745bbccb319
Rudro KHAN <rudrokhan.pro@gmail.com>
2021-04-21 17:29:52 -0700

    Marble mouvement mechanism & fixed show_board

--- mancala.py
+++ mancala.py
@@ -1,6 +1,11 @@
 class Mancala:
     board = [4, 4, 4, 4, 4, 4, 0, 4, 4, 4, 4, 4, 4, 0]
 
+    # Nom des joueurs
+    PLAYER = "PLAYER"
+    BOT = "BOT"
+
+    # Index des mancala par joueur
     PLAYER_MANCALA_INDEX = 6
     BOT_MANCALA_INDEX = 13
 
@@ -11,27 +16,76 @@ class Mancala:
         """
             Intial board :
                 ( 4) ( 4) ( 4) ( 4) ( 4) ( 4)
-            ( 0)                               ( 0)
+            ( 0)                             ( 0)
                 ( 4) ( 4) ( 4) ( 4) ( 4) ( 4)
         """
-        player_pits = self.board[self.PLAYER_MANCALA_INDEX +
-                                 1:self.BOT_MANCALA_INDEX]
+        player_pits = self.board[:Mancala.PLAYER_MANCALA_INDEX]
 
-        bot_pits = self.board[:self.PLAYER_MANCALA_INDEX]
+        bot_pits = self.board[Mancala.PLAYER_MANCALA_INDEX +
+                              1:Mancala.BOT_MANCALA_INDEX]
+        bot_pits.reverse()
 
-        mancalas = self.board[self.BOT_MANCALA_INDEX], self.board[
-            self.PLAYER_MANCALA_INDEX]
+        mancalas = self.board[Mancala.BOT_MANCALA_INDEX], self.board[
+            Mancala.PLAYER_MANCALA_INDEX]
 
         print("    (%2s) (%2s) (%2s) (%2s) (%2s) (%2s)" % tuple(bot_pits))
-        print("(%s)                               (%s)" % mancalas)
+        print("(%2s)                             (%2s)" % mancalas)
         print("    (%2s) (%2s) (%2s) (%2s) (%2s) (%2s)" % tuple(player_pits))
+
+    def move(self, player, pit_number):
+        """ Distribue les billes contenues dans un puit donnée """
+        pit_index = self._get_player_pit_index(player, pit_number)
+
+        # Collecter toutes les billes du puit
+        marble_count = self.board[pit_index]
+        self.board[pit_index] = 0
+
+        print("[{}] Déplacement de {} billes du puit n° {}".format(
+            player.ljust(len(Mancala.PLAYER)), marble_count, pit_number))
+
+        # Pour chaque puit suivant dans le sens anti-horaire,
+        # distribuer 1 bille
+        for i in range(pit_index + 1, pit_index + 1 + marble_count):
+            i_pit_index = i % len(self.board)
+
+            self.board[i_pit_index] += 1
+
+    def _get_player_pit_index(self, player, pit_number):
+        """ Renvoi l'index du puit selon le joueur """
+
+        # Renvoyer une erreur si le numero de puit est invalide
+        if pit_number < 1 or pit_number > 6:
+            raise ValueError(
+                'Invalid pit number %d. Pit number should be between 1 and 6' %
+                pit_number)
+        else:
+            # player: 1 => 0, 6 => 5
+            # bot:    1 => 7, 6 => 12
+            return pit_number - 1 if player == Mancala.PLAYER else Mancala.PLAYER_MANCALA_INDEX + 1 + (
+                pit_number - 1)
 
 
 def main():
-    print('Welcome to Mancala !')
+    print('Bienvenue sur Mancala !')
 
     game = Mancala()
 
+    game.show_board()
+    print("")
+
+    game.move(Mancala.PLAYER, 5)
+    game.show_board()
+    print("")
+
+    game.move(Mancala.BOT, 5)
+    game.show_board()
+    print("")
+
+    game.move(Mancala.PLAYER, 1)
+    game.show_board()
+    print("")
+
+    game.move(Mancala.BOT, 6)
     game.show_board()
 
 
