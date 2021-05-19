a0c48356132ab6332fe7c79cea870be1f3764199
Daniel Sarmiento <gs.daans@gmail.com>
2021-05-19 02:32:13 +0200

    update menu

--- menu.py
+++ menu.py
@@ -1,1 +1,72 @@
-# ceci est un test
+def main():
+    print ("Welcome to Mancala,\n")
+    print ("Please choose the mode you want to play:")
+    print ("[1] Player vs Player")
+    print ("[2] Player vs AI")
+    print ("[0] Quit")
+
+    option = -1
+    while option != 0:
+        option = int(input(">> "))
+
+        if option == 1:
+            print ("You selected Player vs Player \n")
+            # call player vs player mode
+            
+        elif option == 2:
+            menu2()
+
+        elif option == 0:
+            print("See you next time !")
+            exit()            
+            
+        else :
+            print ("invalid option.\n")
+
+     
+
+# Menu 2
+def menu2():
+    print ("You selected Player vs AI \n")
+    print ("Please select your difficulty :")
+    print ("[1] Tutorial")
+    print ("[2] Easy")
+    print ("[3] Medium")
+    print ("[4] Extreme")
+    print ("[9] Back")
+    print ("[0] Quit")    
+ 
+    option = -1
+    while option != 0:
+        option = int(input(">> "))
+
+        if option == 1:
+            print ("You selected Tutorial mode \n")
+            #Call game mode
+            
+        elif option == 2:
+            print ("You selected Easy Mode \n")
+            #Call game mode
+
+        elif option == 3:
+            print ("You selected Medium Mode \n")
+            #Call game mode
+
+        elif option == 4:
+            print ("You selected Extreme Mode")
+            print ("Good luck ! :) \n")
+            #Call game mode
+
+        elif option == 9:
+            main()
+
+        elif option == 0:
+            print("See you next time !")
+            exit()
+        
+
+        else :
+            print ("invalid option.\n")
+
+if __name__ == "__main__":
+	main()
